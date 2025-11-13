d30172bbcac46a264e0fb9158916c54b77519c02
jsierigistage-se <jsierigistage-se@users.noreply.github.com>
2025-11-13 17:08:30 +0000

    Add interface.js, main.rs

---
+++ frontend/web/src/interface.js
@@ -0,0 +1,4 @@
+import { ethers } from 'ethers';
+
+export const usesimple_simple_controller = () => { return {}; };
+
---
+++ frontend/web/src/main.rs
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/chatsafe_fhe/frontend/web/src/main.rs
+// Generated at: 2025-12-03T23:45:45.084Z
+
